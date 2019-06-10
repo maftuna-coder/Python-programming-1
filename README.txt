1ec5c2544a4382958f0aa264a3b9ee8d25290f2b
Maftuna Sharabbaeva <sharabbaevamaftuna@gmail.com>
2019-06-10 13:04:17 +0500

    First Commit

---
+++ README.txt
@@ -0,0 +1,7 @@
+a python function with two parameters x and y which print all even number in range
+between x and y. Then call the function with x = 7 and y = 16 to print on the screen:
+8
+10
+12
+14
+16
---
+++ exercise1solution.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb  6 17:08:38 2019
+
+@author: Maftuna
+"""
+
+def number(x,y):
+    num = []
+    for i in range(x,y):
+        if i%2 == 0:
+             num.append(i)   
+    return num
+
+
+
+
